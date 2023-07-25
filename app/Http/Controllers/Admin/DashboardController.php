355d12bbf6013f0f0361bbec8b75a6bbc7e109e0
dibakar1990 <dkdibakar@gmail.com>
2023-07-26 02:05:28 +0530

    admin dashboard modified

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('backend.dashboard');
+        $totalUserCount = User::where('type','!=',1)->count();
+        $totalBannedUserCount = User::where('type','!=',1)->onlyTrashed()->count();
+        $totalActiveUserCount = User::where('type','!=',1)->where('status',1)->count();
+        $totalInactiveUserCount = User::where('type','!=',1)->where('status',0)->count();
+        return view('backend.dashboard',compact(
+            'totalUserCount',
+            'totalBannedUserCount',
+            'totalActiveUserCount',
+            'totalInactiveUserCount'
+        ));
     }
 }
